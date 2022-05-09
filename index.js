b02533aafdddfb3e39240dcc91e99939b2202196
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-05-08 23:17:19 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const exercicio1 = window.document.querySelector('h1');
+console.log(exercicio1);
+
+const exercicio2 = window.document.getElementById('title');
+console.log(exercicio2);
+
+const exercicio3 = window.document.querySelector(".title");
+console.log(exercicio3);
+
